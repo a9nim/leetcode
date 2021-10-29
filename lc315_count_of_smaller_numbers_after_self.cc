e69208d1b1ef9ea882923314f1ce9f50f21c9072
a9nim <91709515+a9nim@users.noreply.github.com>
2021-10-28 21:40:48 -0700

    Update lc315_count_of_smaller_numbers_after_self.cc

--- lc315_count_of_smaller_numbers_after_self.cc
+++ lc315_count_of_smaller_numbers_after_self.cc
@@ -1,51 +1,52 @@
-class BstNode {
-  public:
-    BstNode(int val) : left_(nullptr), right_(nullptr), value_(val), count_(1), sum_left_count_(0) {}
-    ~BstNode() {
-        delete left_; 
-        delete right_;
-    }
-    
-    // Returns the count of the numbers smaller than `num` in its left sub-tree.
-    int AddNum(int num) {
-        if (num == value_) {
-            ++count_;
-            return sum_left_count_;
-        } else if (num > value_) {
-            if (right_ == nullptr) {
-                right_ = new BstNode(num);
-                return count_ + sum_left_count_;
-            } else {
-                return right_->AddNum(num) + count_ + sum_left_count_;
-            }
-        } else {
-            ++sum_left_count_; 
-            if (left_ == nullptr) {
-                left_ = new BstNode(num); 
-                return 0;
-            } else {
-                return left_->AddNum(num);
-            }
-        }
-    } 
-    
-  private:
-    BstNode* left_; // Owned.
-    BstNode* right_;    // Owned.
-    int value_;
-    int count_;
-    int sum_left_count_;
-};
-
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        std::vector<int> result(nums.size());     
-        BstNode root(nums[nums.size() - 1]);
-        result[nums.size() - 1] = 0;
-        for (int i = nums.size() - 2; i >= 0; --i) {
-            result[i] = root.AddNum(nums[i]); 
-        }
+        std::vector<int> result(nums.size());
+        std::vector<std::pair<int, int>> nums_with_original_indices(nums.size());
+        std::vector<std::pair<int, int>> cache(nums.size());
+        for (int i = 0; i < nums.size(); ++i) {
+            nums_with_original_indices[i] = std::make_pair(nums[i], i); 
+        } 
+        mergeSort(0, nums.size(), nums_with_original_indices, cache, result);
         return result;
+    }
+    
+    void mergeSort(int begin, int end, std::vector<std::pair<int, int>>& nums_with_original_indices, std::vector<std::pair<int, int>>& cache, std::vector<int>& result) {
+        if (begin + 1 >= end) {
+            return;
+        }
+        int mid = (begin + end) / 2;
+        mergeSort(begin, mid, nums_with_original_indices, cache, result);
+        mergeSort(mid, end, nums_with_original_indices, cache, result);
+        merge(begin, mid, end, nums_with_original_indices, cache, result);
+        return;
+    }
+    
+    void merge(int begin, int mid, int end, std::vector<std::pair<int, int>>& nums_with_original_indices, std::vector<std::pair<int, int>>& cache, std::vector<int>& result) {
+        // Copy [begin, end) to cache first.
+        for (int i = begin; i < end; ++i) {
+            cache[i] = nums_with_original_indices[i];
+        }
+        
+        int merged = begin;
+        int left = begin;
+        int right = mid;
+        while (left < mid && right < end) {
+            if (cache[left].first <= cache[right].first) {
+                nums_with_original_indices[merged] = cache[left];
+                result[cache[left].second] += merged - left;
+                ++left;
+            } else {
+                nums_with_original_indices[merged] = cache[right];
+                ++right;
+            }
+            ++merged;
+        }
+        while (left < mid) {
+            nums_with_original_indices[merged] = cache[left];
+            result[cache[left].second] += merged - left;
+            ++left;
+            ++merged;
+        }
     }
 };
